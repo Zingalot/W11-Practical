5e8e67a810b0a1febb95892d3e67d5d6ae386a1b
Toby <tts2@st-andrews.ac.uk>
2017-11-29 22:58:04 +0000

    Testing

--- source/Financial.java
+++ source/Financial.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Financial extends Application{
-    public Financial(int productCode, String name, float versionNumber, float price, ArrayList<Employee> developmentTeam){
+    private boolean personal;
+    public Financial(int productCode, String name, float versionNumber, float price, ArrayList<Employee> developmentTeam, boolean personal){
         super(productCode, name, versionNumber, price, developmentTeam);
+        this.personal = personal;
     }
 }
